bddadb51bf6fe634c21bed0ca7e4e22b4b9dc6a8
syedabdullah912 <33052382+syedabdullah912@users.noreply.github.com>
2019-12-16 13:34:03 +0500

    Problem 1

---
+++ problem1.js
@@ -0,0 +1,13 @@
+(function abc() {
+  var sum = 0;
+  var n = 1000;
+
+  while (n--) {
+    if (n % 3 === 0 || n % 5 === 0) {
+      sum += n;
+    }
+  }
+
+  console.log(sum);
+  return sum;
+}());
